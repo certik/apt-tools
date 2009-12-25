9cc8c38e012101fbce0f1fc86be15654d81d4ea0
Ondrej Certik <ondrej@certik.cz>
2009-12-25 22:43:36 +0100

    Some logic added

--- downgrade_list.py
+++ downgrade_list.py
@@ -1,5 +1,16 @@
 import apt_pkg
 
+def version_too_high(pkg):
+    version_installed = pkg.CurrentVer
+    version_all = pkg.VersionList
+    if len(version_all) > 1:
+        print pkg.Name
+        print "installed version:", version_installed.VerStr
+        print "all versions:"
+        for v in version_all:
+            print " ", v.VerStr
+        print "downloadable:", version_installed.Downloadable
+
 def main():
     print "Init..."
     apt_pkg.InitConfig()
@@ -12,8 +23,11 @@ def main():
     packages_all = sorted(cache.Packages, key=lambda pkg: pkg.Name)
     packages_installed = [pkg for pkg in packages_all \
             if pkg.CurrentState == apt_pkg.CurStateInstalled]
+    packages_downgrade = [pkg for pkg in packages_installed \
+            if version_too_high(pkg)]
     print "number of all packages:      ", len(packages_all)
     print "number of installed packages:", len(packages_installed)
+
 
 if __name__ == "__main__":
     main()
